a10527a7f5ffeb7bd0c5c8260e4aafc5d54eae97
FrewtyPebbles <frewtypebbles@gmail.com>
2024-07-10 13:07:27 -0700

    added json api for admin endpoints

--- gate.js
+++ gate.js
@@ -10,8 +10,56 @@ export const API = {
 			body.kitchen = kitchen
 		if (smoking !== null)
 			body.smoking = smoking
-		await fetch("/search", {
+		return await (await fetch("/search", {
 		body:JSON.stringify(body)
-		})
+		})).json()
 	}
+	async admin_get(password){
+		return (await (await fetch("/admin", {
+			method:"GET",
+			body:JSON.stringify({
+				password:password
+			})
+		})).json())["Success"]
+	}
+	async admin_update(password, id, tier = null, capacity = null, kitchen = null, smoking = null, price = null){
+		body = {password:password, id:id}
+		if (tier !== null)
+			body.tier = tier
+		if (capacity !== null)
+			body.capacity = capacity
+		if (kitchen !== null)
+			body.kitchen = kitchen
+		if (smoking !== null)
+			body.smoking = smoking
+		if (price !== null)
+			body.price = price
+		return (await (await fetch("/admin", {
+			method:"PUT",
+			body:JSON.stringify(body)
+		})).json())["Success"]
+	}
+	async admin_delete(password, id){
+		return (await (await fetch("/admin", {
+			method:"DELETE",
+			body:JSON.stringify({
+				password:password,
+				id:id
+			})
+		})).json())["Success"]
+	}
+	async admin_add(password, tier, capacity, kitchen, smoking, price){
+		return (await (await fetch("/admin", {
+			method:"POST",
+			body:JSON.stringify({
+				password:password,
+				tier:tier,
+				capacity:capacity,
+				kitchen:kitchen,
+				smoking:smoking,
+				price:price
+			})
+		})).json())["Success"]
+	}
+
 }
